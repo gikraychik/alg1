9181113e9734299aafa7c5a6993bb541db49e545
George Kraychik <george.kraychik@gmail.com>
2013-04-09 21:00:25 +0400

    Deleted 9.nlt

--- SRC/build.bat
+++ SRC/build.bat
@@ -2,7 +2,7 @@ echo -----------------------------------------------------------------------
 
 del /Q out.hex
 
-..\TOOLS\nl2hex 0x1800 0x50 0.nlt 1.nlt 2.nlt 3.nlt 4.nlt 5.nlt 6.nlt 7.nlt 8.nlt 9.nlt
+..\TOOLS\nl2hex 0x1800 0x50 0.nlt 1.nlt 2.nlt 3.nlt 4.nlt 5.nlt 6.nlt 7.nlt 8.nlt
 
 pause
 
